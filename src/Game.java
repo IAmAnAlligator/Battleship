3b91d7b3576d7570b0f2b33489d97dcd2d3324ce
IAmAnAlligator <torn90@gmail.com>
2024-03-11 16:33:19 +0300

    Input data validation was added. Some minor fixes.

--- src/Game.java
+++ src/Game.java
@@ -2,51 +2,73 @@ import java.util.*;
 
 public class Game {
 
-    static final Game game = new Game();
-    private final Scanner scanner = new Scanner(System.in);
-    private final Random random = new Random();
-    private final Player player = new Player();
-    private final Player bot = new Player();
+    private final String BATTLESHIP = "Battleship";
+    private final String CRUISER = "Cruiser";
+    private final String DESTROYER = "Destroyer";
+    private final String SUBMARINE = "Submarine";
+
+    static final Game GAME = new Game();
+    private final Scanner SCANNER = new Scanner(System.in);
+    private final Random RANDOM = new Random();
+    private final Player PLAYER = new Player();
+    private final Player BOT = new Player();
 
     public void startGame() {
 
-        System.out.println("Enter your name:");
-        player.setName(scanner.nextLine());
+        System.out.println("Enter your name, please:");
+        PLAYER.setName(SCANNER.nextLine());
 
-        System.out.println("Hello " + player.getName() + "!");
+        System.out.println("Hello " + PLAYER.getName() + "!");
         System.out.println("Enter 1 - Play on random field");
         System.out.println("Enter 2 - Initialize field by your own");
-        System.out.println("Press any button to exit");
-        String choose = scanner.nextLine();
+        System.out.println("Press 0 at any step to exit");
+        String choose = SCANNER.nextLine();
 
         if (choose.equals("1")) {
-            game.initRandomField(player);
+            GAME.initRandomField(PLAYER);
         } else if (choose.equals("2")) {
-            game.showField(player.getField());
-            game.initField(player);
+            GAME.showField(PLAYER.getField());
+            GAME.initField(PLAYER);
         } else {
-            System.exit(0);
+            return;
         }
 
-        System.out.println("You field is: ");
-        showField(player.getField(), player.getGameProcessField());
+        System.out.println("Your field is: ");
+        showField(PLAYER.getField(), PLAYER.getGameProcessField());
 
-        bot.setName("Bot");
-        game.initRandomField(bot);
+        BOT.setName("Bot");
+        GAME.initRandomField(BOT);
 
-        List<String> places = player.getListOfPlaces();
+        List<String> places = PLAYER.getListOfPlaces();
 
-        while (player.isFleetActive() && bot.isFleetActive()) {
+        while (PLAYER.isFleetActive() && BOT.isFleetActive()) {
 
-            System.out.println(player.getName() + " attacks");
-            String place = scanner.nextLine();
-            player.attack(bot, place);
+            System.out.println(PLAYER.getName() + " attacks");
+
+            boolean success = false;
+
+            while (!success) {
+
+                try {
+                    String place = SCANNER.nextLine();
+                    if (place.equals("0")) {
+                        return;
+                    }
+                    PLAYER.attack(BOT, place);
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Incorrect input, try again");
+                }
+
+            }
 
             String shoot = getPlaceForAttack(places);
-            System.out.println(bot.getName() + " attacks " + shoot);
-            bot.attack(player, shoot);
-            showField(player.getField(), player.getGameProcessField());
+            System.out.println(BOT.getName() + " attacks " + shoot);
+            BOT.attack(PLAYER, shoot);
+            showField(PLAYER.getField(), PLAYER.getGameProcessField());
         }
+
+        System.out.println(PLAYER.isFleetActive() ? "Congratulations!\nYou won!" : "You lost!");
 
     }
 
@@ -58,12 +80,20 @@ public class Game {
         char randomLetter;
         String result;
 
-        int range = (int) (Math.random() * (5 - 1) + 1);
+        int range = (int) (Math.random() * (4 - 1) + 1);
 
-        if (range == 4) {
-            int randomPlace = (int) (Math.random() * (places.size() - 1) + 0) + 1;
-            result = places.get(randomPlace);
-            places.remove(randomPlace);
+        if (range == 3) {
+            int size = places.size() - 1;
+
+            if (size == 0) {
+                result = places.get(0);
+                places.remove(0);
+            } else {
+                int randomPlace = (int) (Math.random() * (places.size() - 1) + 0) + 1;
+                result = places.get(randomPlace);
+                places.remove(randomPlace);
+            }
+
         } else {
             randomDigit = (int) (Math.random() * (digits.length() - 1) + 0) + 1;
             randomLetter = letters.charAt(randomDigit);
@@ -74,70 +104,63 @@ public class Game {
 
     }
 
+    private Ship initShip(char[][] playerField, String shipClass, int size) {
+        while (true) {
+            try {
+                String place = SCANNER.nextLine();
+                Ship ship = new Ship(shipClass, size, List.of(place.split(" ")));
+                GAME.addShip(playerField, ship);
+                return ship;
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Incorrect input, try again");
+            }
+        }
+    }
+
     private void initField(Player player) {
 
         char[][] playerField = player.getField();
 
-        System.out.println("Enter coordinates for your Battleship, 4 squares:");
-        String battleshipPlace = scanner.nextLine();
+        System.out.println("Enter coordinates for your " + BATTLESHIP + ", 4 squares:");
+        System.out.println("e.g. A1 B1 C1 D1");
+        Ship battleship = initShip(playerField, BATTLESHIP, 4);
+        GAME.showField(playerField);
 
-        Ship battleship = new Ship("Battleship", 4, List.of(battleshipPlace.split(" ")));
-        game.addShip(playerField, battleship);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your " + CRUISER + ", 3 squares:");
+        Ship cruiserA = initShip(playerField, CRUISER, 3);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your Cruiser, 3 squares:");
-        String cruiserAPlace = scanner.nextLine();
-        Ship cruiserA = new Ship("Cruiser", 3, List.of(cruiserAPlace.split(" ")));
-        game.addShip(playerField, cruiserA);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + CRUISER + ", 3 squares:");
+        Ship cruiserB = initShip(playerField, CRUISER, 3);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your another Cruiser, 3 squares:");
-        String cruiserBPlace = scanner.nextLine();
-        Ship cruiserB = new Ship("Cruiser", 3, List.of(cruiserBPlace.split(" ")));
-        game.addShip(playerField, cruiserB);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your " + DESTROYER + ", 2 squares:");
+        Ship destroyerA = initShip(playerField, DESTROYER, 2);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your Destroyer, 2 squares:");
-        String destroyerAPlace = scanner.nextLine();
-        Ship destroyerA = new Ship("Destroyer", 2, List.of(destroyerAPlace.split(" ")));
-        game.addShip(playerField, destroyerA);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + DESTROYER + ", 2 squares:");
+        Ship destroyerB = initShip(playerField, DESTROYER, 2);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your another Destroyer, 2 squares:");
-        String destroyerBPlace = scanner.nextLine();
-        Ship destroyerB = new Ship("Destroyer", 2, List.of(destroyerBPlace.split(" ")));
-        game.addShip(playerField, destroyerB);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + DESTROYER + ", 2 squares:");
+        Ship destroyerC = initShip(playerField, DESTROYER, 2);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your another Destroyer, 2 squares:");
-        String destroyerCPlace = scanner.nextLine();
-        Ship destroyerC = new Ship("Destroyer", 2, List.of(destroyerCPlace.split(" ")));
-        game.addShip(playerField, destroyerC);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your " + SUBMARINE + ", 1 square:");
+        Ship submarineA = initShip(playerField, SUBMARINE, 1);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your Submarine, 1 square:");
-        String submarineAPlace = scanner.nextLine();
-        Ship submarineA = new Ship("Submarine", 1, List.of(submarineAPlace.split(" ")));
-        game.addShip(playerField, submarineA);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + SUBMARINE + ", 1 square:");
+        Ship submarineB = initShip(playerField, SUBMARINE, 1);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your another Submarine, 1 square:");
-        String submarineBPlace = scanner.nextLine();
-        Ship submarineB = new Ship("Submarine", 1, List.of(submarineBPlace.split(" ")));
-        game.addShip(playerField, submarineB);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + SUBMARINE + ", 1 square:");
+        Ship submarineC = initShip(playerField, SUBMARINE, 1);
+        GAME.showField(playerField);
 
-        System.out.println("Enter coordinates for your another Submarine, 1 square:");
-        String submarineCPlace = scanner.nextLine();
-        Ship submarineC = new Ship("Submarine", 1, List.of(submarineCPlace.split(" ")));
-        game.addShip(playerField, submarineC);
-        game.showField(playerField);
-
-        System.out.println("Enter coordinates for your another Submarine, 1 square:");
-        String submarineDPlace = scanner.nextLine();
-        Ship submarineD = new Ship("Submarine", 1, List.of(submarineDPlace.split(" ")));
-        game.addShip(playerField, submarineD);
-        game.showField(playerField);
+        System.out.println("Enter coordinates for your another " + SUBMARINE + ", 1 square:");
+        Ship submarineD = initShip(playerField, SUBMARINE, 1);
+        GAME.showField(playerField);
 
         player.setFleet(new ArrayList<>(Arrays.asList(
                 battleship,
@@ -156,38 +179,38 @@ public class Game {
 
         char[][] playerField = player.getField();
 
-        String battleshipRandom = random.nextBoolean() ? "1A 1B 1C 1D" : "1G 1H 1I 1J";
-        String cruiserARandom = random.nextBoolean() ? "10A 10B 10C" : "10H 10I 10J";
-        String cruiserBRandom = random.nextBoolean() ? "3C 4C 5C" : "3H 4H 5H";
-        String destroyerARandom = random.nextBoolean() ? "3A 4A" : "3J 4J";
-        String destroyerBRandom = random.nextBoolean() ? "7A 8A" : "7J 8J";
-        String destroyerCRandom = random.nextBoolean() ? "7C 8C" : "7H 8H";
-        String submarineARandom = random.nextBoolean() ? "3E" : "3F";
-        String submarineBRandom = random.nextBoolean() ? "6E" : "6F";
-        String submarineCRandom = random.nextBoolean() ? "8E" : "8F";
-        String submarineDRandom = random.nextBoolean() ? "10E" : "10F";
+        String battleshipRandom = RANDOM.nextBoolean() ? "1A 1B 1C 1D" : "1G 1H 1I 1J";
+        String cruiserARandom = RANDOM.nextBoolean() ? "10A 10B 10C" : "10H 10I 10J";
+        String cruiserBRandom = RANDOM.nextBoolean() ? "3C 4C 5C" : "3H 4H 5H";
+        String destroyerARandom = RANDOM.nextBoolean() ? "3A 4A" : "3J 4J";
+        String destroyerBRandom = RANDOM.nextBoolean() ? "7A 8A" : "7J 8J";
+        String destroyerCRandom = RANDOM.nextBoolean() ? "7C 8C" : "7H 8H";
+        String submarineARandom = RANDOM.nextBoolean() ? "3E" : "3F";
+        String submarineBRandom = RANDOM.nextBoolean() ? "6E" : "6F";
+        String submarineCRandom = RANDOM.nextBoolean() ? "8E" : "8F";
+        String submarineDRandom = RANDOM.nextBoolean() ? "10E" : "10F";
 
-        Ship battleship = new Ship("Battleship", 4, List.of(battleshipRandom.split(" ")));
-        Ship cruiserA = new Ship("Cruiser", 3, List.of(cruiserARandom.split(" ")));
-        Ship cruiserB = new Ship("Cruiser", 3, List.of(cruiserBRandom.split(" ")));
-        Ship destroyerA = new Ship("Destroyer", 2, List.of(destroyerARandom.split(" ")));
-        Ship destroyerB = new Ship("Destroyer", 2, List.of(destroyerBRandom.split(" ")));
-        Ship destroyerC = new Ship("Destroyer", 2, List.of(destroyerCRandom.split(" ")));
-        Ship submarineA = new Ship("Submarine", 1, List.of(submarineARandom.split(" ")));
-        Ship submarineB = new Ship("Submarine", 1, List.of(submarineBRandom.split(" ")));
-        Ship submarineC = new Ship("Submarine", 1, List.of(submarineCRandom.split(" ")));
-        Ship submarineD = new Ship("Submarine", 1, List.of(submarineDRandom.split(" ")));
+        Ship battleship = new Ship(BATTLESHIP, 4, List.of(battleshipRandom.split(" ")));
+        Ship cruiserA = new Ship(CRUISER, 3, List.of(cruiserARandom.split(" ")));
+        Ship cruiserB = new Ship(CRUISER, 3, List.of(cruiserBRandom.split(" ")));
+        Ship destroyerA = new Ship(DESTROYER, 2, List.of(destroyerARandom.split(" ")));
+        Ship destroyerB = new Ship(DESTROYER, 2, List.of(destroyerBRandom.split(" ")));
+        Ship destroyerC = new Ship(DESTROYER, 2, List.of(destroyerCRandom.split(" ")));
+        Ship submarineA = new Ship(SUBMARINE, 1, List.of(submarineARandom.split(" ")));
+        Ship submarineB = new Ship(SUBMARINE, 1, List.of(submarineBRandom.split(" ")));
+        Ship submarineC = new Ship(SUBMARINE, 1, List.of(submarineCRandom.split(" ")));
+        Ship submarineD = new Ship(SUBMARINE, 1, List.of(submarineDRandom.split(" ")));
 
-        game.addShip(playerField, battleship);
-        game.addShip(playerField, cruiserA);
-        game.addShip(playerField, cruiserB);
-        game.addShip(playerField, destroyerA);
-        game.addShip(playerField, destroyerB);
-        game.addShip(playerField, destroyerC);
-        game.addShip(playerField, submarineA);
-        game.addShip(playerField, submarineB);
-        game.addShip(playerField, submarineC);
-        game.addShip(playerField, submarineD);
+        GAME.addShip(playerField, battleship);
+        GAME.addShip(playerField, cruiserA);
+        GAME.addShip(playerField, cruiserB);
+        GAME.addShip(playerField, destroyerA);
+        GAME.addShip(playerField, destroyerB);
+        GAME.addShip(playerField, destroyerC);
+        GAME.addShip(playerField, submarineA);
+        GAME.addShip(playerField, submarineB);
+        GAME.addShip(playerField, submarineC);
+        GAME.addShip(playerField, submarineD);
 
         player.setFleet(new ArrayList<>(Arrays.asList(
                 battleship,
